a483bc0bcf3f2d67680382282b278f519c9d93cb
ronitebul1211 <ronitebul1211@gmail.com>
2020-09-04 11:37:32 +0300

    ADD: pseudo code

--- script.js
+++ script.js
@@ -1,31 +1,18 @@
-/** Data structure - Card */
-function Card(type, patternCssClass, isFlipped){
-  this.type = type;
-  this.patternCssClass = patternCssClass;
-  this.isFlipped = isFlipped;
-}
-
-Card.prototype.getType = function(){
-  return this.type;
-}
-Card.prototype.getPatternCssClass = function(){
-  return this.patternCssClass;
-}
-Card.prototype.isFlipped = function(){
-  return this.isFlipped;
-}
 
 // playMode = {
 //   currentMove: 1 // 1 / 2
 //   // if current move is 2 -> check for match in card id 
 //   // update ui
 //   , 
-//   cards: [1, 1, 2 , 2, 3, 3, 4, 4, 5, 5, 6, 6]
-
-
+//   wrongGuessCounter: NUMBER
+//
 // }
 
-//TODO header:  "new game" , "timer", "levels" themes
+//TODO - start game popup
+//when player reach page for the first time / when user choose to play new game (click btn)
+// take, player name, 
+
+//TODO header:  "new game btn", "hello player name",  wrongGuessCounter, "timer", "level"+"theme" status
 // # levels": “easy” (12)(6 types) , medium (18)(9 types) and hard(24)(12 type)
 // create level -> function for lop create each card twice & push to array loop by level
 // create theme -> cardPattern1 css class hold background var base on theme
@@ -45,9 +32,29 @@ Card.prototype.isFlipped = function(){
 // click on second card - counter 2 (save card in play mode)
 // when counter 2 put overlay for 1 seconds on all screen
 // card match -> stay flip, counter 0 -> update isFlipped = TRUE
-// card not match -> flip again counter 0 -> update isFlipped = FALSE
+// card not match -> flip again counter 0 -> update isFlipped = FALSE, wrong Guess counter ++
 
 //TODO GameOver
 // when all cards isFlipped = TRUE -> game over pop up a “You won!” overlay with a new game button.
-// 
+
+//TODO Ninja:
+// 1. Add flipping animation effect for the card.
+// 2. Add a high score functionality, that will save the name of the person with the least amounts of wrong guesses.
+
+/** Data structure - Card */
+function Card(type, patternCssClass, isFlipped){
+  this.type = type;
+  this.patternCssClass = patternCssClass;
+  this.isFlipped = isFlipped;
+}
+
+Card.prototype.getType = function(){
+  return this.type;
+}
+Card.prototype.getPatternCssClass = function(){
+  return this.patternCssClass;
+}
+Card.prototype.isFlipped = function(){
+  return this.isFlipped;
+}
 
